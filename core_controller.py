7382b3baa262ef8b50180767eb995d5d86a81330
Aaron <sapblatt11@gmail.com>
2023-12-04 22:12:14 -0500

    Added feelres etc

--- core_controller.py
+++ core_controller.py
@@ -9,23 +9,39 @@ import shutil
 
 from chromosome import *
 
-def checkConditional(conditional_index):
-	speed = 10 # TODO :  Make real speed later # true
-	enemy_dist = 50 # true
-	wall_dist = 300 # false
-	bulletDanger = 200 #false
+def findAngle(X, ENEMY_X, ENEMY_DIST):  # Taking the X coordinates of agent and enemy
+    hyp = ENEMY_DIST
+    if ENEMY_X - X > 0:
+        x_dist = ENEMY_X - X
+    else:
+        x_dist = X - ENEMY_X
+    theta = int(math.degrees(math.acos(x_dist / hyp)))
+
+    return theta
+    
+def checkConditional(conditional_index, sensors):
+	print(sensors)
+	speed = sensors[0]
+	enemy_dist = sensors[1]
+	wall_dist= sensors[2]
+	closestBulletDistance = sensors[3]
+	enemy_x = sensors[4]
+	enemy_y = sensors[5]
+	x = sensors[6]
+	y = sensors[7]
 	
 	
-	enemy_dir = getEnemyDirection(Enemy_Dist, Enemy_X, Enemy_Y, X, Y)
+	if enemy_dist == None:
+		enemy_dist = -1
+		enemy_dir = -1
+		enemy_x = -1
+		enemy_y = -1
+		
+	enemy_dir = getEnemyDirection(enemy_dist, enemy_x, enemy_y, x, y)
 	
-	if ai.shotDist(0) > 0:
-		closestBulletDistance = ai.shotDist(0)
-	else:
- 		closestBulletDistance = math.inf
             
-    if enemy_dist == None:
-    	enemy_dist = -1
-	# 16 conditionals 
+
+	# 16 conditionals Core 2.0 in Action
 	conditional_list = ["speed > 6", "speed == 0",
 						"enemy_dist < 50", "enemy_dist > 200", 
 						"enemy_dist < 100 and enemy_dir == 1", 
@@ -39,8 +55,6 @@ def checkConditional(conditional_index):
 	
 	
 	result = eval(conditional_list[conditional_index])
-	#print(conditional_list[conditional_index])
-	#print(result)
 	return result
 	
 def wallBetweenTarget(X, Y, ENEMY_X, ENEMY_Y):
@@ -53,10 +67,10 @@ def getEnemyDirection(Enemy_Dist, Enemy_X, Enemy_Y, X, Y):
 	wallPreEnemy = False
 	shotTolerance = random.randint(-5,5)
 	
-	if Enemy_Dist: 
+	if Enemy_Dist != -1: 
 		xDistToEnemy = Enemy_X - X
 		yDistToEnemy = Enemy_Y - Y
-		theta = findAngle(X, Enemy_X, Enemy_DIST)
+		theta = findAngle(X, Enemy_X, Enemy_Dist)
 		wallPreEnemy = wallBetweenTarget(X,Y, Enemy_X, Enemy_Y)
 		
 	else:
@@ -76,17 +90,100 @@ def getEnemyDirection(Enemy_Dist, Enemy_X, Enemy_Y, X, Y):
 
 def AI_loop():
 	try:
-	
-		if checkConditional(conditional_index):
+		#speed = sensors[0]
+		#enemy_dist = sensors[1]
+		#wall_dist= sensors[2] # minimum wall distance
+		#closestBulletDistance = sensors[3]
+		#enemy_x = sensors[4]
+		#enemy_y = sensors[5]
+		#x = sensors[6]
+		#y = sensors[7]
+		
+		# Self details
+		# handicap
+		ai.setTurnSpeedDeg(20)
+		power = 20
+		ai.setPower(power)
+		
+		speed = int(ai.selfSpeed())
+		X = int(ai.selfX())
+		Y = int(ai.selfY())
+		
+		# get information
+		heading = int(ai.selfHeadingDeg())
+		tracking = int(ai.selfTrackingDeg())
+		
+		# Enemy Details
+		closestShipId = int(ai.closestShipId())
+
+		ENEMY_SPEED = None
+		ENEMY_DIST = None
+		ENEMY_X = None
+		ENEMY_Y = None
+		ENEMY_HEADING = None
+
+		if closestShipId != -1:
+			ENEMY_SPEED = float(ai.enemySpeedId(closestShipId))
+			ENEMY_DIST = float(ai.enemyDistanceId(closestShipId))
+			ENEMY_X = int(ai.screenEnemyXId(closestShipId))
+			ENEMY_Y = int(ai.screenEnemyYId(closestShipId))
+			ENEMY_HEADING = float(ai.enemyHeadingDegId(closestShipId))
+
+		if ai.shotDist(0) > 0:
+			closestBulletDistance = ai.shotDist(0)
+		else:
+			closestBulletDistance = math.inf
+		
+		# store in array so we can easily find the shortest feeler
+		feelers = []
+		frontWall = ai.wallFeeler(500, heading)
+		leftWall = ai.wallFeeler(500, heading + 90)
+		rightWall = ai.wallFeeler(500, heading - 90)
+		trackWall = ai.wallFeeler(500, tracking)
+		rearWall = ai.wallFeeler(500, heading - 180)
+		backLeftWall = ai.wallFeeler(500, heading + 135)
+		backRightWall = ai.wallFeeler(500, heading - 135)
+		frontLeftWall = ai.wallFeeler(500, heading + 45)
+		frontRightWall = ai.wallFeeler(500, heading - 45)
+		rightWall1 = ai.wallFeeler(500, heading - 60)
+		rightWall2 = ai.wallFeeler(500, heading - 120)
+		leftWall1 = ai.wallFeeler(500, heading + 60)
+		leftWall2 = ai.wallFeeler(500, heading + 120)
+		slightRight = ai.wallFeeler(500, heading - 85)
+		slightLeft = ai.wallFeeler(500, heading + 85)
+		feelers.append(frontWall)
+		feelers.append(leftWall)
+		feelers.append(rightWall)
+		feelers.append(trackWall)
+		feelers.append(rearWall)
+		feelers.append(backLeftWall)
+		feelers.append(backRightWall)
+		feelers.append(frontLeftWall)
+		feelers.append(frontRightWall)
+		feelers.append(rightWall1)
+		feelers.append(rightWall2)
+		feelers.append(leftWall1)
+		feelers.append(leftWall2)
+		feelers.append(slightRight)
+		feelers.append(slightLeft)
+		
+		wall_dist = min(feelers)
+        
+		sensors = [speed, ENEMY_DIST, wall_dist, closestBulletDistance, ENEMY_X, ENEMY_Y, X, Y]
+		
+		checkConditional(1, sensors) # TODO Del
+		exit() # TODO Del
+		
+		if checkConditional(conditional_index, sensors):
 			# gene = chrome[conditional_index]
 			# TODO GoTo loop_number
 			pass
 			#print("Loop num: {}".format(loop_number))
-		
+
 	except Exception as e:
 		print("Exception")
 		print(e)
 		ai.quitAI()
 
-print(checkConditional(2))
+
 ai.start(AI_loop, ["-name", "Core!", "-join", "localhost"])
