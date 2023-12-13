713a8acf41105781c6927c068935ca7ccd12a312
Aaron <sapblatt11@gmail.com>
2023-12-13 13:55:14 -0500

    Added a bash script to run server and agents

---
+++ run_core.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Start Server
+echo "Starting Xpilots Server"
+# switchBase 1 = 100% probability to swap bases on death, + teams disables teams
+gnome-terminal -- ./xpilots -map simple.xp -noquit -switchBase 1.0 +teams -maxRoundTime 20 -roundsToPlay 0 -resetOnHuman 5 +limitedLives -maxClientsPerIP 10    
+sleep 4
+# Set the number of instances you want to run
+num_instances=5
+
+# Loop to run multiple instances
+for ((i=1; i<=$num_instances; i++)); do
+    echo "Running instance $i"
+    gnome-terminal -- python3 core_controller.py "$i" &
+    sleep 1  # Optional: Add a small delay between instances if needed
+done
+
+# Wait for all instances to finish
+wait
+
+echo "All $num_instances instances have been started."
